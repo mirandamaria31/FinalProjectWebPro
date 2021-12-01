178da6416d9b1aebd43ee456f366f8c9711fa532
mirandamaria31 <mirandacheliaputri@gmail.com>
2021-12-01 22:45:05 +0800

    mirandamaria31

---
+++ src/Pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navigation from "../Components/Navigation";
+import Footer from "../Components/Footer";
+
+const About = () => {
+    return(
+        <>
+            <Navigation />
+                <div className="container my-5">
+		<h1> Weather Forecast  </h1>	
+                    <p><b> Miranda Marchelia Maria - 105011910083</b></p>
+                </div>
+            <Footer />
+        </>
+    );
+}
+
+export default About;
